0eaafcdf8e1991f375c91aa21a4e1788aee3ff49
GazizAuelbaev <90957946+GazizAuelbaev@users.noreply.github.com>
2022-10-04 12:34:49 +0600

    Add files via upload

---
+++ Strategies/Gun.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy.Strategies
+{
+    internal class Gun: IWeapon
+    {
+        void IWeapon.Shoot()
+        {
+            Console.WriteLine("shoots with a gun");
+        }
+    }
+}
