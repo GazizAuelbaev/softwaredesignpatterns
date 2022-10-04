fdb64fc8f99626e1512c8ab8aa237cccdd23605b
GazizAuelbaev <90957946+GazizAuelbaev@users.noreply.github.com>
2022-10-04 13:17:29 +0600

    Add files via upload

---
+++ Strategy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Strategy;
+using Strategy.Strategies;
+
+Hero hero = new("Knight");
+
+hero.Attack();
+
+hero.SetWeapon(new Sword());
+
+hero.Attack();
+
+hero.SetWeapon(new Stick());
+
+hero.Attack();
+
+Console.ReadLine();
